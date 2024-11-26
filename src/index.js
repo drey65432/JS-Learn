f456118084bbac59588ce94d20c9b81fe39428ab
drey65432 <sbymanalu@gmail.com>
2024-11-26 15:13:36 +0700

    Learn 1

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// console.log('Hello Dreyy');
+// console.log('I Like it Coffe');
+
+// window.alert('Hello Dreyy');
+// window.alert('I Like it Coffe');
+
+document.getElementById("myh1").textContent= 'Hello';
+document.getElementById("myp").textContent= 'I Like Pizza';
